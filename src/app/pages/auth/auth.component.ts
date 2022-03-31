8ef0549468bb8714e5a9f2e27802802c6ebe4581
Sergio Dev <sergio-dev@outlook.com>
2022-03-31 12:59:55 +0200

    Feat: register + log users

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { FireService } from 'src/app/shared/services/fire.service'
+import { LoginData } from 'src/app/shared/models/loginData.interface'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {}
+export class AuthComponent {
+  loginForm!: FormGroup
+  registered: boolean = true
+
+  passwordPattern: RegExp =
+    /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/
+
+  constructor(
+    private fb: FormBuilder,
+    private fireSvc: FireService,
+    private router: Router
+  ) {
+    this.loginForm = this.initForm()
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(this.passwordPattern)]
+      ]
+    })
+  }
+
+  toRegistered(i: number) {
+    i === 1 ? (this.registered = false) : (this.registered = true)
+    console.log(i ? 'sign' : 'login')
+  }
+
+  onLogin(): void {
+    this.registered
+      ? this.fireSvc
+          .login(this.loginForm.value)
+          .then(() => this.router.navigate(['/dashboard']))
+          .catch(error => console.log(error.message))
+      : this.fireSvc
+          .register(this.loginForm.value)
+          .then(() => this.router.navigate(['/dashboard']))
+          .catch(error => console.log(error.message))
+  }
+}
--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { AuthRoutingModule } from './auth-routing.module'
 import { AuthComponent } from './auth.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule]
 })
 export class AuthModule {}
