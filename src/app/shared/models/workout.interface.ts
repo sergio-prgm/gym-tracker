bce2f2fc47eeeed0d9ea904a915a8a32238af09f
Sergio Dev <sergio-dev@outlook.com>
2022-04-01 18:54:43 +0200

    Debug: fix workout interface

--- src/app/shared/models/workout.interface.ts
+++ src/app/shared/models/workout.interface.ts
@@ -1,15 +1,31 @@
+// export interface Workout {
+//   uid: string
+//   workoutName: string
+//   date: Date
+//   exercises: Exercise[]
+// }
+
+// interface Exercise {
+//   exerciseName: string
+//   sets: Sets[]
+// }
+
+// interface Sets {
+//   reps: number
+//   weight: number
+// }
+
 export interface Workout {
-  name: string
-  date: Date
+  uid: string
+  workoutName: string
+  date: string
   exercises: Exercise[]
 }
-
 interface Exercise {
-  name: string
-  combo: Combo[]
+  exerciseName: string
+  sets: Set[]
 }
-
-interface Combo {
+interface Set {
   reps: number
   weight: number
 }
